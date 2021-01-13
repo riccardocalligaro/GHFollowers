1f7866f97b00da3bfcad054bfc07c9e764c20fd0
Riccardo Calligaro <riccardocalligaro@gmail.com>
2021-01-13 18:54:54 +0100

    Initial Commit

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Riccardo on 13/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
