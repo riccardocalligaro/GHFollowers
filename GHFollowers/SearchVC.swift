b2b6b06d89ecf1c7d086a268033e785010f68180
Riccardo Calligaro <riccardocalligaro@gmail.com>
2021-01-13 23:53:37 +0100

    Finish search page UI

--- GHFollowers/SearchVC.swift
+++ GHFollowers/SearchVC.swift
@@ -8,10 +8,60 @@
 import UIKit
 
 class SearchVC: UIViewController {
+    
+    
+    // MARK: - Views
+    
+    let logoImageView = UIImageView()
+    let usernameTextField = GFTextField()
+    let callToActionButton = GFButton(backgroundColor: .systemGreen, title: "Get followers")
 
+    // MARK: - Lifecycle
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // if we called this on view did load the nav bar
+        // wouldn't be hidden when the back button is pressed
+        navigationController?.isNavigationBarHidden = true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .systemYellow
+        
+        view.backgroundColor = .systemBackground
+        
+        configureLogoImageView()
+        configureTextField()
+        configureCallToActionButton()
+    }
+    
+    // MARK: - Views configuration
+    
+    func configureLogoImageView() {
+        view.addSubview(logoImageView)
+        
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        logoImageView.image = UIImage(named: "gh-logo")!
+        
+        logoImageView.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 80)
+        logoImageView.centerX(inView: view)
+        logoImageView.setHeight(200)
+        logoImageView.setWidth(200)
+    }
+    
+    func configureTextField() {
+        view.addSubview(usernameTextField)
+        
+        usernameTextField.anchor(top: logoImageView.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 48, paddingLeft: 50, paddingRight: 50)
+        usernameTextField.setHeight(50)
+    }
+    
+    func configureCallToActionButton() {
+        view.addSubview(callToActionButton)
+        
+        callToActionButton.anchor(left: view.leftAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, right: view.rightAnchor, paddingLeft: 50, paddingBottom: 50, paddingRight: 50)
+        
+        callToActionButton.setHeight(50)
     }
 }
